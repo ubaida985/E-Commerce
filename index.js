77b6ecf4e7ebc41820a79405d51a69c518ad8937
Ubaid <ubaida985@gmail.com>
2022-01-19 02:12:07 +0530

    HTML template complete

--- index.js
+++ index.js
@@ -36,4 +36,62 @@ $(document).ready(function () {
     });
 
 
+    //New Products Owl Carousel
+    $("#newProducts .owl-carousel").owlCarousel({
+        dots: true,
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+
+
+    //Blogs Owl Carousel
+    $("#blogs .owl-carousel").owlCarousel({
+        dots: true,
+        loop: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            }
+        }
+    });
+
+
+    //product quantity section on product page
+    let $quantityUp = $(".qty .qtyUp");
+    let $quantityDown = $(".qty .qtyDown");
+    // let $quantityInput = $(".qty .qtyInput");
+
+    $quantityUp.click(function () {
+        let $quantityInput = $(`.qtyInput[data-id='${$(this).data("id")}']`);
+        if ($quantityInput.val() >= 1 && $quantityInput.val() <= 9) {
+            $quantityInput.val(function (i, oldVal) {
+                return ++oldVal;
+            });
+        }
+    });
+    $quantityDown.click(function () {
+        let $quantityInput = $(`.qtyInput[data-id='${$(this).data("id")}']`);
+        if ($quantityInput.val() <= 10 && $quantityInput.val() >= 2) {
+            $quantityInput.val(function (i, oldVal) {
+                return --oldVal;
+            });
+        }
+    });
+
+
 });
